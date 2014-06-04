0e7135a66d5702ffa35189c5fa511bae99959f77
pvt <pvazteixeira@gmail.com>
2014-06-04 16:03:04 -0400

    removed plots

--- green_cart.m
+++ green_cart.m
@@ -12,10 +12,10 @@ function [ G ] = green_cart(N, delta_x, delta_y)
     [X,Y] = meshgrid(-(delta_x*N/2):delta_x:(delta_x*N/2), -(delta_y*N/2):delta_y:(delta_y*N/2));
     G = 1./sqrt(X.^2 + Y.^2);
     G = (1/max(max(G))).*G;
-    [C,h]=contourf(X,Y,G);
+    %[C,h]=contourf(X,Y,G);
 %     surf(X,Y,G);
-    clabel(C,h);
-    axis equal
+    %clabel(C,h);
+    %axis equal
 
 end
 
