2ab36ee9782f5178c3490cc01029e36ae57523f7
pvt <pvazteixeira@gmail.com>
2014-06-05 13:34:24 -0400

    now building for 3dfls

--- buildjar.sh
+++ buildjar.sh
@@ -2,6 +2,8 @@
 
 lcm-gen -j ../3dfls/3dfls/lcmtypes/hauv_didson_t.lcm
 
+lcm-gen -j ../3dfls/3dfls/lcmtypes/hauv_raw_ping_t.lcm
+
 javac -cp /usr/local/share/java/lcm.jar hauv/*.java
 
 jar cf types.jar hauv/*.class
