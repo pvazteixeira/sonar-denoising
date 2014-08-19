ff6db207213ac81ccf536fe06fba509f73d8cc96
pvt <pvazteixeira@gmail.com>
2014-08-19 15:23:17 -0400

    added sonar points message so that didson enhancer can create and publish it

--- buildjar.sh
+++ buildjar.sh
@@ -4,6 +4,8 @@ lcm-gen -j ../3dfls/3dfls/lcmtypes/hauv_didson_t.lcm
 
 lcm-gen -j ../3dfls/3dfls/lcmtypes/hauv_raw_ping_t.lcm
 
+lcm-gen -j ../3dfls/3dfls/lcmtypes/hauv_sonar_points_t.lcm
+
 javac -cp /usr/local/share/java/lcm.jar hauv/*.java
 
 jar cf types.jar hauv/*.class
