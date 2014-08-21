6f659b41cee1f94525e876bbe7299d433da7df0d
pvt <pvazteixeira@gmail.com>
2014-08-20 20:08:57 -0400

    fixed issue with the sonar frame (was upside down); registration now uses homogeneous transformation matrices (like iSAM); resulting global points still incorrect judging from the resulting point cloud

--- didson_enhancer.m
+++ didson_enhancer.m
@@ -40,6 +40,8 @@ beam = zeros(1,96);
 beam(1,[1 9 17 25 33 41 49 57 65 73 81 89]) =[  24 24 24 27 32 40 70 40 32 27 24 24];
 PSF = (1/sum(sum(beam)))*beam;
 
+window_length_ref = [ 1.125, 2.25, 4.5, 9.0];
+
 while true
     millis_to_wait = 10;
     msg = aggregator.getNextMessage(millis_to_wait);
@@ -49,10 +51,10 @@ while true
                                  
         % frame data
         serialized_image_data = typecast(message_in.m_cData, 'uint8');
-        frame = im2double(flip(reshape(serialized_image_data, 96, 512)')); % deserialize & store
+        frame = im2double((reshape(serialized_image_data, 96, 512)')); % deserialize & store
         window_start = 0.375 * message_in.m_nWindowStart;       
-        window_length = 1.125*(power(2,(message_in.m_nWindowStart)));
-        
+        %window_length = 1.125*(power(2,(message_in.m_nWindowStart)));
+        window_length = window_length_ref(message_in.m_nWindowStart+1);
         
         %% enhance frame
         enhanced_frame = enhance(frame, 0, 0);      
@@ -61,21 +63,21 @@ while true
         %
         
         returns_didson_frame = [];
-        %threshold = 100/255;    % IMPORTANT : replace with something better (e.g. mean + N*stddev)
-        threshold = max(0.4, mean(enhanced_frame(:)) + 4.5 * sqrt(var(enhanced_frame(:))));
+        threshold = 110/255;    % IMPORTANT : replace with something better (e.g. mean + N*stddev)
+        threshold = max(0.43, mean(enhanced_frame(:)) + 3 * sqrt(var(enhanced_frame(:))));
         hold off;
         imshow(enhanced_frame);
         hold on;
         
-        for beam = 0:95
+        for beam = 1:96
             % find max in beam
-            [value, index] = max(enhanced_frame(:, beam + 1));          
+            [value, index] = max(enhanced_frame(:, beam));          
             if value > threshold;
                 % if the return exceeds the threshold, map it in the sonar frame
-                plot(beam+1, index, 'r.');
-                range = window_start + window_length * (index/512);
+                plot(beam, index, 'r.');
+                range = window_start + window_length * ((index)/512);
                 theta = beam_width * (48 - beam);
-                returns_didson_frame = [returns_didson_frame, [range*cos(theta); range*sin(theta); 0]];
+                returns_didson_frame = [returns_didson_frame, [range*cos(theta); range*sin(theta); 0; 1]];
             end
              
         end
@@ -87,27 +89,30 @@ while true
         %% register returns 
         
         % didson pose in the platform frame (m_pose_didson_local in didson_cv)
-        didson_position = [ message_in.m_fSonarXOffset; message_in.m_fSonarYOffset; message_in.m_fSonarZOffset; ];
+        didson_position = [ message_in.m_fSonarXOffset; 
+                            message_in.m_fSonarYOffset; 
+                            message_in.m_fSonarZOffset; ];
         didson_pan = deg2rad(message_in.m_fSonarPan + message_in.m_fSonarPanOffset);
         didson_tilt = deg2rad(message_in.m_fSonarTilt + message_in.m_fSonarTiltOffset);
         didson_roll = deg2rad(message_in.m_fSonarRoll + message_in.m_fSonarRollOffset);
-        R_didson_local = angle2dcm(didson_pan, didson_tilt, didson_roll);
-
-        returns_local = zeros(3, return_count);        
-     
+        R_didson_local = angle2dcm(didson_pan, didson_tilt, didson_roll)';
+        T_didson_local = [  R_didson_local, didson_position;
+                            zeros(1,3),     1;];
+        returns_local = T_didson_local*returns_didson_frame;
+                        
         % platform pose in the global frame (m_pose_local_global in didson_cv)
-        local_position = [message_in.m_fSonarX; message_in.m_fSonarY; message_in.m_fSonarZ;];
+        local_position = [  message_in.m_fSonarX; 
+                            message_in.m_fSonarY; 
+                            message_in.m_fSonarZ;];
         local_heading = deg2rad(message_in.m_fHeading);
         local_pitch = deg2rad(message_in.m_fPitch);
         local_roll = deg2rad(message_in.m_fRoll);       
-        R_local_global = angle2dcm(local_heading, local_pitch, local_roll);
-        
-        returns_global = zeros(3, return_count);
-        for i = 1:return_count
-            returns_local(:,i) = didson_position + R_didson_local*returns_didson_frame(:,i);
-            returns_global(:,i) = local_position + R_local_global*returns_local(:,i);
-        end
+        R_local_global = angle2dcm(local_heading, local_pitch, local_roll)';
+        T_local_global = [  R_local_global, local_position;
+                            zeros(1,3),     1;];
+        returns_global = T_local_global*returns_local;
              
+        
         %% publish returns
         msg_out = hauv.sonar_points_t();
         
@@ -116,8 +121,8 @@ while true
         msg_out.orientation = [ y, p, r, 0];
 
         msg_out.n = int32(return_count);
-        msg_out.points_global = returns_global';
-        msg_out.points_local = returns_local';
+        msg_out.points_global = returns_global(1:3,:)';
+        msg_out.points_local = returns_local(1:3,:)';
         
         lc.publish('SONAR_POINTS', msg_out);
         
