84b6a25f70df7474e0f8614efd7567de6fcc183e
pvt <pvazteixeira@gmail.com>
2014-09-04 17:46:22 -0400

    adding function to simplify transform computation

---
+++ getTransform.m
@@ -0,0 +1,11 @@
+function [ aTb ] = getTransform( b_position_a, attitude )
+%UNTITLED2 Get homogenous transform matrix from b to a
+%   b_position_a - position of the b frame's origin in the a frame
+%   attitude     - rotation from a frame to b frame, in ypr sequence
+
+    aRb= angle2dcm(attitude(1),attitude(2),attitude(3),);
+    
+    aTb = [ aRb;        b_position_a;
+            zeros(1,3), 1];
+end
+
