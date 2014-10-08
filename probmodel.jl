514b43c9a635e0fe97a34c447a936e157811b731
pvazteixeira <pvazteixeira@gmail.com>
2014-10-07 23:58:58 -0400

    fixed switched probabilities

--- probmodel.jl
+++ probmodel.jl
@@ -1,6 +1,9 @@
-function sonarpmodel(N, p_tp, p_tn)
+function sonarpmodel(N, p_tp, p_tn )
   #p_tp = 0.9;  # probability for a true positive
   #p_tn = 0.99;  # probability for a true negative
+
+  p_fp = 1 - p_tn;
+  p_fn = 1 - p_tp;
 
   p_o = zeros(N,1);
   p_o[(N-200):end] = 1;
@@ -12,12 +15,13 @@ function sonarpmodel(N, p_tp, p_tn)
 
     if (range>1)
       for i=1:1:(range-1)
-        p_misses = p_misses*( (1-p_tn)*p_o[i] + p_tn*(1-p_o[i]) );
+        p_misses = p_misses*( p_fn*p_o[i] + p_tn*(1-p_o[i]) );
       end
     end
 
-    pr[range] = ( p_tp*p_o[range] + (1-p_tp)*(1-p_o[range]) )*p_misses
+    pr[range] = ( p_tp*p_o[range] + p_fp*(1-p_o[range]) )*p_misses
   end
 
+  display(sum(pr))
   return pr
 end
