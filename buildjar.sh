76f4b41cabf183b839f7cea8c4c7f2e03d7d188d
pvazteixeira <pvazteixeira@gmail.com>
2014-10-25 23:49:11 -0400

    generating jars for new lcm types

--- buildjar.sh
+++ buildjar.sh
@@ -1,11 +1,14 @@
 #!/bin/sh
 
-lcm-gen -j ../3dfls/3dfls/lcmtypes/hauv_didson_t.lcm
+# 'standard' hauv lcm types
+lcm-gen -j ../3dfls/3dfls/lcmtypes/hauv_didson_t.lcm       # didson
+lcm-gen -j ../3dfls/3dfls/lcmtypes/hauv_raw_ping_t.lcm     # 3dfls
+lcm-gen -j ../3dfls/3dfls/lcmtypes/hauv_sonar_points_t.lcm # registered sonar returns
 
-lcm-gen -j ../3dfls/3dfls/lcmtypes/hauv_raw_ping_t.lcm
+# occupancy grid mapping
+lcm-gen -j ../ogmapper/lcmtypes/hauv_sonar_range_t.lcm     # single beam
+lcm-gen -j ../ogmapper/lcmtypes/hauv_sonar_scan_t.lcm      # set/collection of beams (didson/3dfls)
 
-lcm-gen -j ../3dfls/3dfls/lcmtypes/hauv_sonar_points_t.lcm
-
+# 
 javac -cp /usr/local/share/java/lcm.jar hauv/*.java
-
 jar cf types.jar hauv/*.class
