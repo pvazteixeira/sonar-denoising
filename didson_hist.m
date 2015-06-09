8c2d44feccc8fb70fd4240d477b33d219e0ce534
pvazteixeira <pvazteixeira@gmail.com>
2015-06-09 15:25:29 -0400

    adding script to show histograms and help figuring out didson gains

---
+++ didson_hist.m
@@ -0,0 +1,55 @@
+% DIDSON_HIST.M A live histogram viewer for the DIDSON sonar
+%
+% The purpose of this script is to listen for DIDSON frames and compute the
+% respective histograms, to help determining the best sonar gain.
+%
+% Pedro Vaz Teixeira (PVT), June 2015
+% pvt@mit.edu
+
+%% Clean up
+close all;  % close any open figures
+clc;        % clear the console
+clear;
+addjars;
+
+%% LCM
+lc = lcm.lcm.LCM.getSingleton();
+aggregator = lcm.lcm.MessageAggregator();
+lc.subscribe('HAUV_DIDSON_FRAME', aggregator);  % subscribe to didson frames
+
+%% Initialization
+message_count = 0;
+frame_reduction_factor = 1; % increase if playing logs faster than real time
+
+%% Main processing loop
+while true
+  millis_to_wait = 1;
+  msg = aggregator.getNextMessage(millis_to_wait);
+
+  if ~isempty(msg)
+    message_count = message_count + 1;      % increase message counter;
+    if ( rem(message_count, frame_reduction_factor) == 0 )
+      tic
+      
+      message_in = hauv.didson_t(msg.data);   % got a new message
+      serialized_image_data = typecast(message_in.m_cData, 'uint8');  % frame data
+      frame = im2double((reshape(serialized_image_data, 96, 512)));   % deserialize & store
+      enhanced_frame = enhance(frame, 0, 0);    
+  
+      figure(2); clf;
+      histogram(frame(:),25);
+      hold on
+      histogram(enhanced_frame(:),25)
+      grid on
+      xlim([0 1])
+      set(gca,'YScale','log')
+      % the following lines are commented out
+%     legend('raw','enhanced')
+%     xlabel('intensity')
+%     ylabel('count')
+      drawnow;       
+
+      toc
+    end
+  end
+end
